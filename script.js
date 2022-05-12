e5a52cd596ab6dc2ef5cc902512d5ab08f5d502e
Marllon Batista <marllonbatista.12@gmail.com>
2022-05-12 11:55:40 -0300

    last commit

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const menuOpen= document.getElementById('open-menu');
+
 function openMenu(){
     const menu= document.getElementById('menu-mobile');
+    const body= document.getElementById('body');
     menu.classList.toggle('active');
+    body.classList.toggle('active');
 }
 menuOpen.addEventListener('click',openMenu);
